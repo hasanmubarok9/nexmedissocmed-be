04515f0409839bbc46f4de4aa8928d4f7a921016
Hasan Mubarok <hasanmubarok32@gmail.com>
2025-04-11 22:11:03 +0700

    feat: init posts rest api

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot()
+    ConfigModule.forRoot(),
+    PrismaModule,
+    PostsModule
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/posts/dto/create-post.dto.ts
@@ -0,0 +1,5 @@
+export class CreatePostDto {
+  content: string;
+  imageUrl?: string;
+  userId: number;
+}
